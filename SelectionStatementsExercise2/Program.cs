423a114755421ef8862c723dcf956e4353412865
Donald Johnson <donaldrjohnson66@gmail.com>
2022-11-20 20:20:52 -0600

    Finished

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+
+            var subject = Console.ReadLine();
+
+            FavSubject(subject);
+
+        }
+        public static void FavSubject(string subject)
+        {
+             
+            
+            switch (subject.ToLower())
+            {
+                case "math":
+                    
+                        Console.WriteLine("Good Choice!");
+                        break;
+                    
+                case "science":
+                    
+                        Console.WriteLine("Good Choice!");
+                        break;
+                    
+                case "history":
+                    
+                        Console.WriteLine("Good Choice!");
+                        break;
+                    
+                case "english":
+                    
+                        Console.WriteLine("Good Choice!");
+                        break;
+                    
+                case "reading":
+                    
+                        Console.WriteLine("Good Choice!");
+                        break;
+                    
+                default:
+                    
+                        Console.WriteLine("I see.");
+                        break;
+                    
+                
+                
+                                 
+
+            }
+            
         }
     }
 }
